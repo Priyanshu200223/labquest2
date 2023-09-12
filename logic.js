67913f6d7a19105defb1d9c2afa6e7d0f6e0ded7
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-09-12 17:45:48 +0530

    initial commit

---
+++ logic.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elementhead = document.querySelector(".head");
+    const elementsubh = document.querySelector(".subh");
+    const elementheroimage = document.querySelector(".heroimage");
+    elementsubh.classList.add("fade-in-right");
+    elementhead.classList.add("fade-in-left");
+    elementheroimage.classList.add("zoom-in");
+});
