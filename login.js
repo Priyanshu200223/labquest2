7d0b94568741c4ab0d55bad4d44f07f0811f3904
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-09-13 18:56:30 +0530

    inti

---
+++ login.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDJPkMheOnWSJnMIozfsQcdA2Scsd5P8Q",
+    authDomain: "labquest-3aea4.firebaseapp.com",
+    projectId: "labquest-3aea4",
+    storageBucket: "labquest-3aea4.appspot.com",
+    messagingSenderId: "637763619838",
+    appId: "1:637763619838:web:5f1674f6cb716765f0ceaf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
