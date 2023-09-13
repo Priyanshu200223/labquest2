033593ac17f39de75524a0cbe9ba29b4bd8c966d
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-09-13 19:09:18 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 
-  // Import the functions you need from the SDKs you need
+  
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
+ 
   const firebaseConfig = {
     apiKey: "AIzaSyCDJPkMheOnWSJnMIozfsQcdA2Scsd5P8Q",
     authDomain: "labquest-3aea4.firebaseapp.com",
@@ -14,5 +14,5 @@
     appId: "1:637763619838:web:5f1674f6cb716765f0ceaf"
   };
 
-  // Initialize Firebase
+
   const app = initializeApp(firebaseConfig);
